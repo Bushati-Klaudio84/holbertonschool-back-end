f8dd7f5ce1be1b2248a192870df4e90f7a918d64
Bushati-Klaudio84 <klaudio1@gmail.com>
2024-08-29 18:43:02 +0200

    c

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-""" Module that starts a Flask web application """
 from flask import Flask
 
 
@@ -11,12 +9,19 @@ def hello_route():
     """ Function that returns a string when / route is requested """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
     """ Function that returns a string when / route is requested """
     return ' HBNB!'
 
 
+@app.route('/c/text', strict_slashes=False)
+def c_print(text):
+    """ Function that returns a string when / route is requested """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    
